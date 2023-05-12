3995ceb4396cf1f0c4c03a91d75df1d08263375f
David Renteria <david.j.renteria1@gmail.com>
2023-05-12 14:33:04 -0500

    working ad show page

--- src/main/java/com/codeup/adlister/controllers/ArbitraryAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ArbitraryAdServlet.java
@@ -4,15 +4,18 @@ import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.Ad;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+@WebServlet(name="ArbitraryAdServelet", urlPatterns = "/ads/index")
 public class ArbitraryAdServlet extends HttpServlet {
     protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        int arbitraryAdId = Integer.parseInt(request.getParameter("id"));
-        Ad ad = DaoFactory.getAdsDao().findAdById(arbitraryAdId);
+        Long arbitraryAd = Long.parseLong(request.getParameter("id"));
+        Ad arbitAd = DaoFactory.getAdsDao().findAdById(arbitraryAd);
+        request.setAttribute("arbitraryAd", DaoFactory.getAdsDao().findAdById(arbitraryAd));
+        request.getRequestDispatcher("/WEB-INF/ads/arbitrayad.jsp").forward(req, response);
 
 
     }
--- src/main/webapp/WEB-INF/ads/index.jsp
+++ src/main/webapp/WEB-INF/ads/index.jsp
@@ -15,7 +15,9 @@
             <h2>${ad.title}</h2>
             <p>${ad.description}</p>
             <form action="/ads/arbitrary" method="get">
-                <a href="/ad?id=${Ad.id}">Show more info</a>
+                <label for="arbitraryAd">Type in the name of the game you'd like to view"</label>
+                <input id="arbitraryAd" name="arbitraryAd" type="text" value="${ad.title}">
+                <button type="submit">Clieck to view game details!</button>
             </form>
             <form action="/ads/create" method="get">
                 <a href=""></a>
